b986713190913af6d24d5ea180e2e9d1ee0c2ef8
Carsten Igel <cig@bite-that-bit.de>
2020-11-22 22:05:14 +0100

    Documentation added

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,5 +1,19 @@
+/*
+ * lib.rs - (C) 2020 by Carsten Igel
+ * 
+ * Published using the MIT License
+ */
+
+#![deny(missing_docs)]
+#![warn(missing_doc_code_examples)]
+#![warn(missing_crate_level_docs)]
+
+//! Crate documentation
+
+/// Testing module
 #[cfg(test)]
 mod tests {
+    /// Function to test this in general
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
