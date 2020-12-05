1ca2e24789200d3bd92baf793890e54d9be505c7
Carsten Igel <cig@bite-that-bit.de>
2020-12-05 14:10:36 +0100

    Applied formatting rules and formatting for rust

--- rs/src/file_system/perms.rs
+++ rs/src/file_system/perms.rs
@@ -1,37 +1,37 @@
 /*
  * perms.rs - (C) 2020 by Carsten Igel
- * 
+ *
  * Published using the MIT License
  */
 
-use libc::{S_IRUSR, S_IWUSR, S_IXUSR, S_ISUID, S_IRGRP, S_IWGRP, S_IXGRP, S_ISGID, S_IROTH, S_IWOTH, S_IXOTH, S_ISVTX};
-use std::path::PathBuf;
-use std::os::unix::fs::PermissionsExt;
-use libc::{mode_t};
+use libc::mode_t;
+use libc::{
+    S_IRGRP, S_IROTH, S_IRUSR, S_ISGID, S_ISUID, S_ISVTX, S_IWGRP, S_IWOTH,
+    S_IWUSR, S_IXGRP, S_IXOTH, S_IXUSR,
+};
 use std::convert::TryFrom;
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
 
-pub struct PermissionSet
-{
+pub struct PermissionSet {
     user_permissions: super::nibble::Nibble,
     group_permissions: super::nibble::Nibble,
-    other_permissions: super::nibble::Nibble
+    other_permissions: super::nibble::Nibble,
 }
 
-const PERMISSION_NONE    : super::nibble::Nibble = super::nibble::Nibble::B0000;
-const PERMISSION_READ    : super::nibble::Nibble = super::nibble::Nibble::B0001;
-const PERMISSION_WRITE   : super::nibble::Nibble = super::nibble::Nibble::B0010;
-const PERMISSION_EXECUTE : super::nibble::Nibble = super::nibble::Nibble::B0100;
-const PERMISSION_SPECIAL : super::nibble::Nibble = super::nibble::Nibble::B1000;
+const PERMISSION_NONE: super::nibble::Nibble = super::nibble::Nibble::B0000;
+const PERMISSION_READ: super::nibble::Nibble = super::nibble::Nibble::B0001;
+const PERMISSION_WRITE: super::nibble::Nibble = super::nibble::Nibble::B0010;
+const PERMISSION_EXECUTE: super::nibble::Nibble = super::nibble::Nibble::B0100;
+const PERMISSION_SPECIAL: super::nibble::Nibble = super::nibble::Nibble::B1000;
 
-impl PermissionSet
-{
-    pub fn encode(self: &PermissionSet) -> u16
-    {
+impl PermissionSet {
+    pub fn encode(self: &PermissionSet) -> u16 {
         let mut result = 0u16;
 
-        let user_perms : u16 = self.user_permissions.to_u4().into();
-        let group_perms : u16 = self.group_permissions.to_u4().into();
-        let other_perms : u16 = self.other_permissions.to_u4().into();
+        let user_perms: u16 = self.user_permissions.to_u4().into();
+        let group_perms: u16 = self.group_permissions.to_u4().into();
+        let other_perms: u16 = self.other_permissions.to_u4().into();
 
         result = result + user_perms;
         result = result << 4;
@@ -39,63 +39,82 @@ impl PermissionSet
         result = result << 4;
         result = result + other_perms;
         result = result & 0x0FFFu16;
-        
+
         return result;
     }
 }
 
-fn unwrap_permissions(mode: mode_t, read_perm: mode_t, write_perm: mode_t, exec_perm: mode_t, special: mode_t) -> super::nibble::Nibble
-{
-    let mut permission : super::nibble::Nibble = PERMISSION_NONE;
+fn unwrap_permissions(
+    mode: mode_t,
+    read_perm: mode_t,
+    write_perm: mode_t,
+    exec_perm: mode_t,
+    special: mode_t,
+) -> super::nibble::Nibble {
+    let mut permission: super::nibble::Nibble = PERMISSION_NONE;
 
-    if mode & read_perm == read_perm
-    {
+    if mode & read_perm == read_perm {
         permission = permission | PERMISSION_READ;
     }
 
-    if mode & write_perm == write_perm
-    {
+    if mode & write_perm == write_perm {
         permission = permission | PERMISSION_WRITE;
     }
 
-    if mode & exec_perm == exec_perm
-    {
+    if mode & exec_perm == exec_perm {
         permission = permission | PERMISSION_EXECUTE;
     }
 
-    if mode & special == special
-    {
+    if mode & special == special {
         permission = permission | PERMISSION_SPECIAL;
     }
 
     return permission;
 }
 
-pub fn get_permissions(path: PathBuf) -> Result<PermissionSet, super::FsError>
-{
+pub fn get_permissions(path: PathBuf) -> Result<PermissionSet, super::FsError> {
     let file_metadata = path.symlink_metadata();
-    match file_metadata
-    {
+    match file_metadata {
         Ok(metadata) => {
-            let permission_set_mode : u32 = metadata.permissions().mode();
+            let permission_set_mode: u32 = metadata.permissions().mode();
 
             let permission_set_result = mode_t::try_from(permission_set_mode);
 
             match permission_set_result {
-                Err(_) => { return Err(super::FsError::UnknownError); },
+                Err(_) => {
+                    return Err(super::FsError::UnknownError);
+                }
                 Ok(permission_set) => {
-                    let user_permissions = unwrap_permissions(permission_set, S_IRUSR, S_IWUSR, S_IXUSR, S_ISUID);
-                    let group_permissions = unwrap_permissions(permission_set, S_IRGRP, S_IWGRP, S_IXGRP, S_ISGID);
-                    let other_permissions = unwrap_permissions(permission_set, S_IROTH, S_IWOTH, S_IXOTH, S_ISVTX);
+                    let user_permissions = unwrap_permissions(
+                        permission_set,
+                        S_IRUSR,
+                        S_IWUSR,
+                        S_IXUSR,
+                        S_ISUID,
+                    );
+                    let group_permissions = unwrap_permissions(
+                        permission_set,
+                        S_IRGRP,
+                        S_IWGRP,
+                        S_IXGRP,
+                        S_ISGID,
+                    );
+                    let other_permissions = unwrap_permissions(
+                        permission_set,
+                        S_IROTH,
+                        S_IWOTH,
+                        S_IXOTH,
+                        S_ISVTX,
+                    );
 
                     return Ok(PermissionSet {
                         user_permissions,
                         group_permissions,
-                        other_permissions
+                        other_permissions,
                     });
                 }
-            } 
-        },
+            }
+        }
         Err(_) => {
             return Err(super::FsError::UnknownError);
         }
--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,6 +1,6 @@
 /*
  * lib.rs - (C) 2020 by Carsten Igel
- * 
+ *
  * Published using the MIT License
  */
 
@@ -15,18 +15,15 @@ use std::path::PathBuf;
 
 mod file_system;
 
-enum FileSystemQueryResult 
-{
+enum FileSystemQueryResult {
     Ok = 0,
     NoResult = 1,
     ParameterIsNull = 17,
     StringConversionError = 18,
 }
 
-fn fsqr_as_u16(value: FileSystemQueryResult) -> u16
-{
-    match value
-    {
+fn fsqr_as_u16(value: FileSystemQueryResult) -> u16 {
+    match value {
         FileSystemQueryResult::Ok => 0u16,
         FileSystemQueryResult::NoResult => 1u16,
         FileSystemQueryResult::ParameterIsNull => 17u16,
@@ -36,125 +33,139 @@ fn fsqr_as_u16(value: FileSystemQueryResult) -> u16
 
 #[no_mangle]
 /// External
-pub extern "C" fn sys_get_maximum_login_name() -> i32
-{
+pub extern "C" fn sys_get_maximum_login_name() -> i32 {
     return _SC_LOGIN_NAME_MAX.into();
 }
 
 #[no_mangle]
 /// External
-pub extern "C" fn fs_owning_user_name(fs_entry: * const c_char, fs_user_name : *mut c_char) -> u16
-{
-    if fs_entry.is_null()
-    {
+pub extern "C" fn fs_owning_user_name(
+    fs_entry: *const c_char,
+    fs_user_name: *mut c_char,
+) -> u16 {
+    if fs_entry.is_null() {
         return fsqr_as_u16(FileSystemQueryResult::ParameterIsNull);
     }
 
-    if fs_user_name.is_null()
-    {
+    if fs_user_name.is_null() {
         return fsqr_as_u16(FileSystemQueryResult::ParameterIsNull);
     }
 
     let file_path_res = file_system::marshal::c_string_to_str(fs_entry);
-    
-    match file_path_res
-    {
-        Some(file_path) => { 
-            let path : PathBuf = PathBuf::from(file_path);
-            let user_result : Result<String, file_system::FsError> = file_system::owner::user(path);
-            match user_result
-            {
-                Ok(user) => { 
+
+    match file_path_res {
+        Some(file_path) => {
+            let path: PathBuf = PathBuf::from(file_path);
+            let user_result: Result<String, file_system::FsError> =
+                file_system::owner::user(path);
+            match user_result {
+                Ok(user) => {
                     if file_system::marshal::str_to_c_string(user, fs_user_name)
                     {
                         return fsqr_as_u16(FileSystemQueryResult::Ok);
                     }
 
-                    return fsqr_as_u16(FileSystemQueryResult::StringConversionError);
-                },
-                Err(_) => { return fsqr_as_u16(FileSystemQueryResult::NoResult); }
+                    return fsqr_as_u16(
+                        FileSystemQueryResult::StringConversionError,
+                    );
+                }
+                Err(_) => {
+                    return fsqr_as_u16(FileSystemQueryResult::NoResult);
+                }
             }
-        },
-        None => { return fsqr_as_u16(FileSystemQueryResult::StringConversionError); }
+        }
+        None => {
+            return fsqr_as_u16(FileSystemQueryResult::StringConversionError);
+        }
     }
 }
 
 #[no_mangle]
 /// External
-pub extern "C" fn fs_owning_group_name(fs_entry: * const c_char, fs_group_name : *mut c_char) -> u16
-{
-    if fs_entry.is_null()
-    {
+pub extern "C" fn fs_owning_group_name(
+    fs_entry: *const c_char,
+    fs_group_name: *mut c_char,
+) -> u16 {
+    if fs_entry.is_null() {
         return fsqr_as_u16(FileSystemQueryResult::ParameterIsNull);
     }
 
-    if fs_group_name.is_null()
-    {
+    if fs_group_name.is_null() {
         return fsqr_as_u16(FileSystemQueryResult::ParameterIsNull);
     }
 
     let file_path_res = file_system::marshal::c_string_to_str(fs_entry);
 
-    match file_path_res
-    {
-        Some(file_path) => { 
-            let path : PathBuf = PathBuf::from(file_path);
-            let group_result : Result<String, file_system::FsError> = file_system::owner::group(path);
-            match group_result
-            {
-                Ok(group) => { 
-                    if file_system::marshal::str_to_c_string(group, fs_group_name)
-                    {
+    match file_path_res {
+        Some(file_path) => {
+            let path: PathBuf = PathBuf::from(file_path);
+            let group_result: Result<String, file_system::FsError> =
+                file_system::owner::group(path);
+            match group_result {
+                Ok(group) => {
+                    if file_system::marshal::str_to_c_string(
+                        group,
+                        fs_group_name,
+                    ) {
                         return fsqr_as_u16(FileSystemQueryResult::Ok);
                     }
 
-                    return fsqr_as_u16(FileSystemQueryResult::StringConversionError);
-                },
-                Err(_) => { return fsqr_as_u16(FileSystemQueryResult::NoResult); }
+                    return fsqr_as_u16(
+                        FileSystemQueryResult::StringConversionError,
+                    );
+                }
+                Err(_) => {
+                    return fsqr_as_u16(FileSystemQueryResult::NoResult);
+                }
             }
-        },
-        None => { return fsqr_as_u16(FileSystemQueryResult::StringConversionError); }
+        }
+        None => {
+            return fsqr_as_u16(FileSystemQueryResult::StringConversionError);
+        }
     }
 }
 
 #[no_mangle]
 /// External
-pub extern "C" fn fs_permissions(fs_entry: * const c_char, permission : *mut u16) -> u16
-{
-    if fs_entry.is_null()
-    {
+pub extern "C" fn fs_permissions(
+    fs_entry: *const c_char,
+    permission: *mut u16,
+) -> u16 {
+    if fs_entry.is_null() {
         return fsqr_as_u16(FileSystemQueryResult::ParameterIsNull);
     }
 
-    if permission.is_null()
-    {
+    if permission.is_null() {
         return fsqr_as_u16(FileSystemQueryResult::ParameterIsNull);
     }
 
     let file_path_res = file_system::marshal::c_string_to_str(fs_entry);
 
-    match file_path_res
-    {
-        Some(file_path) => { 
-            let path : PathBuf = PathBuf::from(file_path);
-            let permission_result : Result<file_system::perms::PermissionSet, file_system::FsError> = file_system::perms::get_permissions(path);
-            match permission_result
-            {
+    match file_path_res {
+        Some(file_path) => {
+            let path: PathBuf = PathBuf::from(file_path);
+            let permission_result: Result<
+                file_system::perms::PermissionSet,
+                file_system::FsError,
+            > = file_system::perms::get_permissions(path);
+            match permission_result {
                 Ok(perm) => {
                     unsafe {
-                        * permission = perm.encode();
+                        *permission = perm.encode();
                     }
 
-                    return fsqr_as_u16(FileSystemQueryResult::Ok); 
-                },
-                Err(_err) => { return fsqr_as_u16(FileSystemQueryResult::NoResult); }
+                    return fsqr_as_u16(FileSystemQueryResult::Ok);
+                }
+                Err(_err) => {
+                    return fsqr_as_u16(FileSystemQueryResult::NoResult);
+                }
             }
-        },
-        None => { return fsqr_as_u16(FileSystemQueryResult::StringConversionError); }
+        }
+        None => {
+            return fsqr_as_u16(FileSystemQueryResult::StringConversionError);
+        }
     }
 }
-
-
 
 /// Testing module
 #[cfg(test)]
